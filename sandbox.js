28f38eb4cb6c5848dc62d57acab1cd75c8583fd8
Jerud-Moyer <jerudmoyer@gmail.com>
2020-10-01 14:43:52 -0700

    one step closer

--- sandbox.js
+++ sandbox.js
@@ -4,14 +4,23 @@ const T = new Twit({
   consumer_key: process.env.APPLICATION_CONSUMER_KEY_HERE,
   consumer_secret: process.env.APPLICATION_CONSUMER_SECRET_HERE,
   access_token: process.env.ACCESS_TOKEN_HERE,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET_HERE
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET_HERE,
+  
 });
 const latestTweet = '';
-const { data } = T.get('statuses/user_timeline', { screen_name: 'realdonaldtrump', count:1 })
-  .then(response => console.log(response.data[0].text));
+const { data } = T.get('statuses/user_timeline', { screen_name: 'GregMall3', count:1 })
+  .then(response => console.log(response.data[0].text, response.data[0].id));
 
 // const stream = T.stream('statuses/sample');
 
 // stream.on('tweet', (tweet) => {
 //   console.log(tweet.text);
 // });
+
+// T.post('statuses/update', { status: 'hello world!' }, (err, data, response) => {
+//   console.log(data);
+// });
+
+T.post('statuses/update', { status: 'something' }, (err, data, response) => {
+  console.log(data);
+});
