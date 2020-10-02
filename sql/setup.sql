651186ff3608cab8bf05efbd8099698ab82bb71f
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-02 13:33:08 -0700

    Sql table created for states, model created

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,10 +2,17 @@ DROP TABLE IF EXISTS states;
 
 CREATE TABLE states (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  state TEXT, 
-  name TEXT,
-  election_info TEXT, 
-  registration_url TEXT,
-  voting_location TEXT, 
-  ballot_info TEXT
+  state TEXT NOT NULL,
+  reg_url TEXT NOT NULL,
+  check_reg_url TEXT NOT NULL,
+  ab_req_url TEXT,
+  online_reg_deadline DATE NOT NULL,
+  mail_reg_deadline DATE NOT NULL,
+  in_person_reg_deadline DATE NOT NULL,
+  ab_req_deadline DATE NOT NULL,
+  ab_ret_mail_deadline DATE NOT NULL,
+  ab_in_person_deadline DATE NOT NULL,
+  early_voting_start DATE,
+  postmark BOOLEAN 
+  
 );
