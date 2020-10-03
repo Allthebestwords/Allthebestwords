9d4b8952ef0fb8100ccbf669e2c7f68a40b56d8c
Brian McPhillips <B.McPhillips89@gmail.com>
2020-10-02 17:06:21 -0700

    scraper almost working

--- sql/setup.sql
+++ sql/setup.sql
@@ -6,13 +6,13 @@ CREATE TABLE states (
   reg_url TEXT NOT NULL,
   check_reg_url TEXT NOT NULL,
   ab_req_url TEXT,
-  online_reg_deadline DATE NOT NULL,
-  mail_reg_deadline DATE NOT NULL,
-  in_person_reg_deadline DATE NOT NULL,
-  ab_req_deadline DATE NOT NULL,
-  ab_ret_mail_deadline DATE NOT NULL,
-  ab_in_person_deadline DATE NOT NULL,
-  early_voting_start DATE,
+  online_reg_deadline TEXT NOT NULL,
+  mail_reg_deadline TEXT NOT NULL,
+  in_person_reg_deadline TEXT NOT NULL,
+  ab_req_deadline TEXT NOT NULL,
+  ab_ret_mail_deadline TEXT NOT NULL,
+  ab_in_person_deadline TEXT NOT NULL,
+  early_voting_start TEXT,
   postmark BOOLEAN 
   
 );
