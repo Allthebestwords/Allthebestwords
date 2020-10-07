154e0a1c2f03bc8ce5da5a84fee32cfc36bba90f
Jerud-Moyer <jerudmoyer@gmail.com>
2020-10-07 15:45:12 -0700

    testing

--- __tests__/request-parser.test.js
+++ __tests__/request-parser.test.js
@@ -1,7 +1,12 @@
 const State = require('../lib/models/state');
 const requestParser = require('../lib/services/request-parser');
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
 
 describe('test request-parser', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.SQL', 'utf-8'));
+  });
   it('should return a state object given a state name', async() => {
     const Oregon = await State.findById('OR');
     const response = await requestParser('Oregon');
