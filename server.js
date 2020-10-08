93202f925274b638e120cdfc85a1ab9d8c29a72f
Greg Mall <gregmall157@gmail.com>
2020-10-07 18:02:26 -0700

    getting server set up

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
 
 const PORT = process.env.PORT || 7890;
+const twitBot = require('./lib/twitter/twit');
+
+const seed = require('./lib/data/seed-data');
+twitBot();
+seed();
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
